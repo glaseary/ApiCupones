fafda90b38b064596e315e7649d14b96664e442c
glaseary <sofia.troncoso.m@gmail.com>
2025-06-14 23:40:56 -0400

    primer commit

---
+++ src/main/java/com/Perfulandia/ApiCupones/ApiCuponesApplication.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiCupones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCuponesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCuponesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Perfulandia/ApiCupones/ApiCuponesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandia.ApiCupones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCuponesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
